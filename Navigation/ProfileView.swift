5e32da1398d3fd43f2ecfa7b398ae1affbb4f3ef
Alexey Kalinin <jaketyler_95@mail.ru>
2023-01-16 02:22:29 +0300

    HW 1.2

--- Navigation/ProfileView.swift
+++ Navigation/ProfileView.swift
@@ -5,4 +5,14 @@
 //  Created by Алексей Калинин on 15.01.2023.
 //
 
-import Foundation
+import UIKit
+
+class ProfileView: UIView {
+
+    @IBOutlet weak var photo: UIImageView!
+    @IBOutlet weak var name: UILabel!
+    @IBOutlet weak var birthday: UILabel!
+    @IBOutlet weak var city: UILabel!
+    @IBOutlet weak var about: UITextView!
+
+}
--- Navigation/ProfileViewController.swift
+++ Navigation/ProfileViewController.swift
@@ -5,4 +5,20 @@
 //  Created by Алексей Калинин on 15.01.2023.
 //
 
-import Foundation
+import UIKit
+
+class ProfileViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        let width = UIScreen.main.bounds.width
+        let height = UIScreen.main.bounds.height
+
+        if let profileView = Bundle.main.loadNibNamed("ProfileView", owner: nil, options: nil)?.first as? ProfileView {
+            profileView.frame = CGRect(x: 0, y: 20, width: width, height: height)
+            view.addSubview(profileView)
+        }
+    }
+}
+
